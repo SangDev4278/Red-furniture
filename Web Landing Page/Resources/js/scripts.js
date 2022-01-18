4ff1c11905b15e4f71dbe8910d11b4ac9cf8024e
NguyenNhuSang <genjishimada1601@gmail.com>
2022-01-18 20:22:21 +0700

    none

---
+++ Web Landing Page/Resources/js/scripts.js
@@ -0,0 +1,30 @@
+{/* <script>
+        var CORRECT_ADMIN_NAME = 'Sang';
+        var CORRECT_ADMIN_EMAIL = 'admin@gmail.com';
+        var CORRECT_ADMIN_PASS = 'admin1';
+
+        var inputAdmin = document.getElementById('adminName');
+        var inputAdminemail = document.getElementById('adminEmail');
+        var inputAdminpass = document.getElementById('adminPass');
+
+        var adminLog = document.getElementById('adminLog');
+
+        if(adminLog.attachEvent) {
+            adminLog.attachEvent('submit', onAdsubmit);
+        } 
+        else {
+            adminLog.addEventListener('submit', onAdsubmit)
+        }
+
+        function onAdsubmit {
+            var adminName = inputAdmin;
+            var adminEmail = inputAdminemail;
+            var adminPassword = inputAdminpass;
+
+            if(adminName == CORRECT_ADMIN_NAME && adminEmail == CORRECT_ADMIN_EMAIL && adminPassword == correct CORRECT_ADMIN_PASS) {
+                window.location.href = './admin.html;
+            }
+            else {
+                alert('Failed !')
+            }
+    </script>  */}
